62209c0bfd7ecdb1df4c26faba0a89fbf3bd5f7e
Nikolay Benlioglu <nikbenlioglu@gmail.com>
2024-04-03 17:23:34 +0300

    Add background image to MainLayout.

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import { Outlet } from 'react-router-dom'; // Import Outlet from react-router-dom
-
+import LayoutBackgroundImage from '../assets/layout-background-image.png';
 import { Box, PaletteMode } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import AppAppBar from '../components/AppAppBar';
 
 export default function MainLayout() {
-  const [mode, setMode] = React.useState<PaletteMode>('light');
-  const defaultTheme = createTheme({ palette: { mode } });
+  const [mode, setMode] = React.useState<PaletteMode>('dark');
+  const defaultTheme = createTheme({
+     palette: { mode },
+     components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            backgroundImage: `url(${LayoutBackgroundImage})`,
+          }
+        }
+      }
+    }
+   });
 
   const toggleColorMode = () => {
     setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
